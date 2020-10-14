b966540a29e67577f1722fe2927931f8017b2b20
ShixiangWang <wangshx@shanghaitech.edu.cn>
2020-10-14 15:24:16 +0800

    make run_docker.sh work

--- run_docker.sh
+++ run_docker.sh
@@ -1,25 +1,34 @@
 #!/usr/bin/env bash
-CURRENT_DIR=$(cd `dirname $0`; pwd)
+# ShixiangWang@2020
+# w_shixiang@163.com
+
+GISTIC_LOC=/opt/GISTIC          # DONT change this position, it refer to docker path
+OUTDIR=`pwd`/test_results       # Change it if necessary
 
 echo --- creating output directory ---
-#basedir=`pwd`/<set_your_result_directory_here>
-basedir=`pwd`/test_results
+#basedir=<set_your_result_directory_here>
+basedir=$OUTDIR
 mkdir -p $basedir
+echo output directory is $basedir
 
 echo --- running GISTIC ---
+echo Set input file paths...
 ## input file definitions
 #segfile=/path/to/your/input.seg
-segfile=`pwd`/examplefiles/segmentationfile.txt
+segfile=$GISTIC_LOC/examplefiles/segmentationfile.txt
 
 ## reference file
-refgenefile=`pwd`/refgenefiles/hg38.UCSC.add_miR.160920.refgene.mat
+refgenefile=$GISTIC_LOC/refgenefiles/hg19.mat
 # Available reference list
 # hg16.mat/hg17.mat/hg18.mat/hg19.mat
 # hg19.UCSC.add_miR.140312.refgene.mat
 # hg38.UCSC.add_miR.160920.refgene.mat
 
-sudo docker run --rm -v $CURRENT_DIR:/opt/GISTIC/ \
-  shixiangwang/gistic -b $basedir -seg $segfile -refgene $refgenefile \
+echo Starting...
+DOCKER_OUTDIR="$GISTIC_LOC"/run_result
+
+sudo docker run --rm -v $basedir:$DOCKER_OUTDIR \
+  shixiangwang/gistic -b $DOCKER_OUTDIR -seg $segfile -refgene $refgenefile \
   -rx 0 -genegistic 1 -smallmem 1 -broad 1 -brlen 0.5 -twosize 1 \
   -armpeel 1 -savegene 1 -maxseg 10000 -conf 0.99
 
